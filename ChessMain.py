41a424c88c1caea4a6cbbf61f8b1d4ce52cf89aa
Jainel Liz Boban <jainel.boban@ucdconnect.ie>
2025-08-04 19:09:08 +0100

    Simplify code by removing timer related changes

--- ChessMain.py
+++ ChessMain.py
@@ -6,7 +6,6 @@ import ChessEngine, ChessAI
 
 WIDTH = 512 
 HEIGHT = 512
-HEADER_HEIGHT = 30 # bar for the timer
 DIMENSION = 8
 SQ_SIZE = HEIGHT // DIMENSION
 MAX_FPS = 20
@@ -26,7 +25,7 @@ Initializing main. Handles user input and updating graphics
 """
 def main():
     p.init() # Initialize the pygame module
-    screen = p.display.set_mode((WIDTH, HEIGHT + HEADER_HEIGHT)) # Set the window size
+    screen = p.display.set_mode((WIDTH, HEIGHT)) # Set the window size
     clock = p.time.Clock() # Create a clock object
     game_state = ChessEngine.GameState() # Create a GameState object calling the Gamestate __init_ method
     legal_moves = game_state.get_valid_moves() # Get the valid moves for the current game state
@@ -42,18 +41,9 @@ def main():
     promotion_pending_move = None
     human_white_player = False # Flag for white player (True if human, False if AI)
     human_black_player = False # Flag for black player (True if human, False if AI)
-    # timer bar
-    white_time_seconds = 300  # 5 minutes
-    black_time_seconds = 300  # 5 minutes
-    timer_font = p.font.SysFont("Helvitca", 20, True, False)
 
     while is_running:
-        time_delta = clock.tick(MAX_FPS) / 1000.0
-        if not is_game_over and not promotion_pending_move:
-            if game_state.white_to_move:
-                white_time_seconds -= time_delta
-            else:
-                black_time_seconds -= time_delta
+        clock.tick(MAX_FPS)
         # If human player is playing, set human_turn to True
         human_turn = (game_state.white_to_move and human_white_player) or (not game_state.white_to_move and human_black_player)
 
@@ -63,10 +53,8 @@ def main():
             elif event.type == p.MOUSEBUTTONDOWN: # Check if the user clicked the mouse
                 if not is_game_over and not promotion_pending_move and human_turn: # If the game is not over, no promotion pending, and it's the human player's turn
                     click_position = p.mouse.get_pos() # Get the mouse (x,y) position
-                    # Adjust click position for the header
-                    if click_position[1] < HEADER_HEIGHT: continue
                     clicked_col = click_position[0] // SQ_SIZE # Calculate the column based on mouse position
-                    clicked_row = (click_position[1] - HEADER_HEIGHT) // SQ_SIZE # Calculate the row based on mouse position
+                    clicked_row = click_position[1] // SQ_SIZE # Calculate the row based on mouse position
                     
                     if selected_square == (clicked_row, clicked_col): # If the same square is clicked twice
                         selected_square, click_history = (), [] # deselect the selected square
@@ -115,8 +103,7 @@ def main():
                         legal_moves = game_state.get_valid_moves()
                         selected_square, click_history = (), [] # Undo any square selections
                         move_executed, should_animate, is_game_over = False, False, False
-                        white_time_seconds, black_time_seconds = 300, 300
-
+        
         # AI move logic
         if not is_game_over and not promotion_pending_move and not human_turn: # If the game is not over, no promotion pending, and it's the AI's turn
             AI_move = ChessAI.get_best_minimax_move(game_state, legal_moves) # Get the best move from the Best Move AI function
@@ -130,8 +117,7 @@ def main():
             legal_moves = game_state.get_valid_moves() # Get the valid moves for the current game state
             move_executed, should_animate = False, False # Reset the flags
 
-        # Pass timer variables to drawing function
-        draw_game_state(screen, game_state, legal_moves, selected_square, promotion_pending_move, white_time_seconds, black_time_seconds, timer_font)
+        draw_game_state(screen, game_state, legal_moves, selected_square, promotion_pending_move)
         
         if game_state.is_checkmate: # If checkmated, gameover and print checkmate message
             is_game_over = True
@@ -140,25 +126,16 @@ def main():
         elif game_state.is_stalemate:
             is_game_over = True
             draw_text(screen, 'Stalemate!')
-        elif white_time_seconds <= 0:
-            is_game_over = True
-            draw_text(screen, 'Black wins on time!')
-        elif black_time_seconds <= 0:
-            is_game_over = True
-            draw_text(screen, 'White wins on time!')
 
         p.display.flip() # Update the display
 
 """
 Graphics in the current game state
 """
-# Accept timer variables to pass them along to the timer drawing function
-def draw_game_state(screen, game_state, legal_moves, selected_square, promotion_pending_move, white_time, black_time, font):
-    p.draw.rect(screen, p.Color("gainsboro"), p.Rect(0, 0, WIDTH, HEADER_HEIGHT)) # Draw header background
+def draw_game_state(screen, game_state, legal_moves, selected_square, promotion_pending_move):
     draw_board(screen) # Draw the squares on board
     highlight_squares(screen, game_state, legal_moves, selected_square, promotion_pending_move) # Highlight current chess piece and its valid moves
     draw_pieces(screen, game_state.board) # Draw the pieces on the squares on board
-    draw_timer(screen, font, white_time, black_time, game_state.white_to_move) # Draw the timer
 
 """
 Highlighting the selected square and promotion choice
@@ -170,13 +147,13 @@ def highlight_squares(screen, game_state, legal_moves, selected_square, promotio
         s = p.Surface((SQ_SIZE, SQ_SIZE))
         s.set_alpha(120)  # semi-transparent
         s.fill(p.Color('red'))
-        screen.blit(s, (king_pos[1] * SQ_SIZE, king_pos[0] * SQ_SIZE + HEADER_HEIGHT))
+        screen.blit(s, (king_pos[1] * SQ_SIZE, king_pos[0] * SQ_SIZE))
 
     if promotion_pending_move:
         s = p.Surface((SQ_SIZE, SQ_SIZE))
         s.set_alpha(150)
         s.fill(p.Color('blue'))
-        screen.blit(s, (promotion_pending_move.end_col * SQ_SIZE, promotion_pending_move.end_row * SQ_SIZE + HEADER_HEIGHT))
+        screen.blit(s, (promotion_pending_move.end_col * SQ_SIZE, promotion_pending_move.end_row * SQ_SIZE))
         return
 
     if selected_square != ():
@@ -186,12 +163,12 @@ def highlight_squares(screen, game_state, legal_moves, selected_square, promotio
             s = p.Surface((SQ_SIZE, SQ_SIZE)) # Create a surface for highlighting
             s.set_alpha(100) # Setting transparency level 100 is fully opaque
             s.fill(p.Color("orange")) # Fill the surface with orange color
-            screen.blit(s, (col * SQ_SIZE, row * SQ_SIZE + HEADER_HEIGHT)) # Draw the highlight on the selected square
+            screen.blit(s, (col * SQ_SIZE, row * SQ_SIZE)) # Draw the highlight on the selected square
             s.fill(p.Color('orange')) # Fill valid move surfaces to orange color
             for move in legal_moves:
                 if move.start_row == row and move.start_col == col:
                     # Draw the highlight on the selected valid movement squares
-                    screen.blit(s, (move.end_col * SQ_SIZE, move.end_row * SQ_SIZE + HEADER_HEIGHT))
+                    screen.blit(s, (move.end_col * SQ_SIZE, move.end_row * SQ_SIZE))
 
 """
 Draws the squares on the chess board.
@@ -201,7 +178,7 @@ def draw_board(screen):
     for row in range(DIMENSION):  # Loop through each row
         for col in range(DIMENSION): # Loop through each column
             color = colors[((row + col) % 2)]  # Alternate colors for squares
-            p.draw.rect(screen, color, p.Rect(col * SQ_SIZE, row * SQ_SIZE + HEADER_HEIGHT, SQ_SIZE, SQ_SIZE))  # Draw the square
+            p.draw.rect(screen, color, p.Rect(col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE))  # Draw the square
 
 """
 Animating chess piece movement
@@ -213,20 +190,19 @@ def animate_move(move, screen, board, clock):
     frame_count = (abs(row_delta) + abs(col_delta)) * frames_per_square  # total frame count b/w rows and cols
     for frame in range(frame_count + 1): # Going through each frame
         row, col = (move.start_row + row_delta * frame / frame_count, move.start_col + col_delta * frame / frame_count)
-        p.draw.rect(screen, p.Color("gainsboro"), p.Rect(0, 0, WIDTH, HEADER_HEIGHT))
         draw_board(screen)
         draw_pieces(screen, board)
         
         # Erasing piece moved from ending square (As it is already handled in GameState - avoiding duplicate piece and endSquare)
         color = colors[(move.end_row + move.end_col) % 2]
-        end_square = p.Rect(move.end_col * SQ_SIZE, move.end_row * SQ_SIZE + HEADER_HEIGHT, SQ_SIZE, SQ_SIZE)
+        end_square = p.Rect(move.end_col * SQ_SIZE, move.end_row * SQ_SIZE, SQ_SIZE, SQ_SIZE)
         p.draw.rect(screen, color, end_square) # Replacing piece with square color
         
         # Draw the captured piece on top of the rectangle drawn
         if move.captured_piece != '--': screen.blit(IMAGES[move.captured_piece], end_square)
         
         # Drawing moving piece
-        screen.blit(IMAGES[move.moved_piece], p.Rect(col * SQ_SIZE, row * SQ_SIZE + HEADER_HEIGHT, SQ_SIZE, SQ_SIZE))
+        screen.blit(IMAGES[move.moved_piece], p.Rect(col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE))
         p.display.flip()
         clock.tick(60) # 60 fps (60 frames per sec - for one square movement 1/6 sec)
 
@@ -238,30 +214,7 @@ def draw_pieces(screen, board):
         for col in range(DIMENSION):  # Loop through each column
             piece = board[row][col]  # Get the piece at the current square
             if piece != "--":  # If there is a piece on the square (not empty)
-                screen.blit(IMAGES[piece], p.Rect(col * SQ_SIZE, row * SQ_SIZE + HEADER_HEIGHT, SQ_SIZE, SQ_SIZE))  # Draw the piece
-
-"""
-Draws the timer at the top of the window
-"""
-def draw_timer(screen, font, white_time, black_time, white_to_move):
-    white_time = max(0, white_time)
-    black_time = max(0, black_time)
-    
-    white_mins, white_secs = divmod(white_time, 60)
-    black_mins, black_secs = divmod(black_time, 60)
-    
-    white_str = f"{int(white_mins):02}:{int(white_secs):02}"
-    black_str = f"{int(black_mins):02}:{int(black_secs):02}"
-    
-    white_color = p.Color('darkgreen') if white_to_move else p.Color('black') # Highlight the active player's timer
-    black_color = p.Color('darkgreen') if not white_to_move else p.Color('black')
-
-    white_text = font.render(white_str, True, white_color)
-    black_text = font.render(black_str, True, black_color)
-
-    # Position timers
-    screen.blit(white_text, white_text.get_rect(midleft=(10, HEADER_HEIGHT // 2)))
-    screen.blit(black_text, black_text.get_rect(midright=(WIDTH - 10, HEADER_HEIGHT // 2)))
+                screen.blit(IMAGES[piece], p.Rect(col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE))  # Draw the piece
 
 """
 Draws text for game over messages
@@ -269,8 +222,8 @@ Draws text for game over messages
 def draw_text(screen, text):
     font = p.font.SysFont("Helvitca", 32, True, False) # Bold, not italic, 32 size, Helvetica
     text_surface = font.render(text, 0, p.Color('Gray')) # Render in black color with no aliasing
-    text_rect = p.Rect(0, 0, WIDTH, HEIGHT + HEADER_HEIGHT).move(WIDTH/2 - text_surface.get_width()/2, 
-                                                    (HEIGHT + HEADER_HEIGHT)/2 - text_surface.get_height()/2) # Centering the text on the board
+    text_rect = p.Rect(0, 0, WIDTH, HEIGHT).move(WIDTH/2 - text_surface.get_width()/2, 
+                                                                 HEIGHT/2 - text_surface.get_height()/2) # Centering the text on the board
     screen.blit(text_surface, text_rect)
     text_surface = font.render(text, 0, p.Color("Black"))
     screen.blit(text_surface, text_rect.move(2, 2))
